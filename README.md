8cd8e98da29da2da152a0017b36b1e3f10b07bca
Andras Nagy <contact@andras.me>
2019-10-19 15:22:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Issue demo
+
+https://github.com/jantimon/html-webpack-plugin/issues/1293
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.ejs' 
+    })
+  ]
+}
